d18c2263860ea6ef7dd02bc70ddfcd70629e7c95
Firzinat Khuzeev <firzinat.khuzeev@akvelon.com>
2017-10-25 11:02:26 +0300

    Add project files.

---
+++ GetDate.ConsoleApp/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace GetDate.ConsoleApp
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("'g' to Get Date; 'gc' to Garbage Collect; 'x' to Exit");
+            var command = "";
+            while (command != "x")
+            {
+                command = Console.ReadLine();
+                switch (command)
+                {
+                    case "g":
+                        GetDate();
+                        break;
+
+                    case "gc":
+                        GC.Collect();
+                        break;
+                }
+            }
+        }
+
+        private static void GetDate()
+        {
+            //using (var databaseState = new UnmanagedDatabaseState())
+            var databaseState = new UnmanagedDatabaseState();
+            {
+                Console.WriteLine(databaseState.GetDate());
+            }
+        }
+    }
+}
